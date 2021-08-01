8b416b0d082985ab9902e7ba7d3d54da028454d6
rberduo <berduoronald@gmail.com>
2021-08-01 11:23:59 -0600

    Update time of intervalTest

--- reflex-agent/index.js
+++ reflex-agent/index.js
@@ -37,7 +37,7 @@ const $imgTrashB = $imgTrashA.cloneNode();
 
 const intervalTest = setInterval(() => {
   test();
-}, 3000);
+}, 2000);
 const intervalDirty = setInterval(() => {
   randomDirty();
 }, 6000);
